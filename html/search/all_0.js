6fca825c95bfb51cae026288ef4e4c953c80fc87
Skarain96 <mega-smail-96@mail.ru>
2014-06-03 12:03:46 +0400

    Added Laba_3_stack

---
+++ html/search/all_0.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['peek',['peek',['../classstack.html#aec2fb50fc7f8667b7da9b4024fc2d879',1,'stack']]],
+  ['pop',['pop',['../classstack.html#afd6bf8357a2139cb5c2e03253d9caf96',1,'stack']]],
+  ['print',['print',['../classstack.html#a90fd9f1887a3b49022d651ea24e54956',1,'stack']]],
+  ['push',['push',['../classstack.html#a1b860caf499b1da44e57048e1c287e14',1,'stack']]]
+];
---
+++ Лаба3-стек/Исходный код.cpp
@@ -0,0 +1,129 @@
+﻿/**
+@class stack
+@brief  
+Класс, реализующий стек элементов.
+@note
+Содержит методы добавления элемента в вершину стека, удаления вершины, получения значения вершины и вывода всех элементов стека на экран.
+*/
+
+#include <iostream>
+using namespace std;
+
+class stack
+{
+private:
+	class item
+	{
+	private:
+		int info;
+	public:
+		item* next;
+		item(int info_, item* next_)
+		{
+			this->info = info_;
+			this->next = next_;
+		}
+		~item()
+		{
+		}
+		int get()
+		{
+			return this->info;
+		}
+	};
+	item* head;
+public:
+	stack();
+	~stack();
+	void push(int a);
+	void pop();
+	int peek();
+	void print();
+};
+
+/**
+@brief
+Конструктор.
+*/
+
+stack::stack()
+{
+	this->head = nullptr;
+}
+
+/**
+@brief
+Деструктор.
+*/
+
+stack::~stack()
+{
+	while (this->head != nullptr)
+	{
+		this->pop();
+	}
+}
+
+/**
+@brief
+Добавление элемента в вершину.
+@param a
+Значение элемента.
+@brief  p
+Вспомогательный указатель.
+*/
+
+void stack::push(int a)
+{
+	item * p = new item(a, this->head);
+	this->head = p;
+}
+
+/**
+@brief
+Удаление элемента вершины.
+*/
+
+void stack::pop()
+{
+	item* p = this->head;
+	this->head = p->next;
+	p->~item();
+}
+
+/**
+@brief
+Получение элемента вершины.
+*/
+
+int stack::peek()
+{
+	return this->head->get();
+}
+
+/**
+@brief
+Вывод на экран всех элементов стека.
+*/
+
+void stack::print()
+{
+	while (this->head != nullptr)
+	{
+		std::cout << this->peek() << " ";
+		this->pop();
+	}
+}
+
+void main()
+{
+	int N, ak;
+	std::cin >> N;
+	stack A = stack();
+	for (int i = 0; i < N; i++)
+	{
+		std::cin >> ak;
+		A.push(ak);
+	}
+	A.print();
+}
